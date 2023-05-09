393ac7c7c9fc90e98be307d49aad8fc4a6ecdb5e
AnnaPaulish <apaulish99@gmail.com>
2023-05-09 17:31:35 +0200

    part 4 fix num of iterations

--- src/ddpg_agent.py
+++ src/ddpg_agent.py
@@ -8,16 +8,16 @@ class DDPGAgent:
         self.noisy_action = noisy_action
 
     def compute_action(self, state, device, deterministic=True):
-        with torch.no_grad():
-            if not torch.is_tensor(state):
-                state  = torch.FloatTensor(state).to(device)
-            action = self.policy_network(state)  # Process state with the policy network to get an action
-            #action = action.detach().cpu().numpy()
+        
+        if not torch.is_tensor(state):
+            state  = torch.FloatTensor(state).to(device)
+        action = self.policy_network(state)  # Process state with the policy network to get an action
+        #action = action.detach().cpu().numpy()
 
-            if not deterministic:
-                # action = action.detach().cpu().numpy()
-                            
-                    action = self.noisy_action.get_noisy_action(action[0])
+        if not deterministic:
+            # action = action.detach().cpu().numpy()
+                        
+                action = self.noisy_action.get_noisy_action(action[0])
         return action
     
 # action_noise = GaussianActionNoise(0.5)
